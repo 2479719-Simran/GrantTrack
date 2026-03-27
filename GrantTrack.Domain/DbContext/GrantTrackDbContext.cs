f20f046b05ba9a4c99a2972d6cdad47f346a18fb
Sadayappan Arunachalam <sadayappan3101@gmail.com>
2026-03-27 12:14:36 +0530

    Removed connection string from dbContext

--- GrantTrack.Domain/DbContext/GrantTrackDbContext.cs
+++ GrantTrack.Domain/DbContext/GrantTrackDbContext.cs
@@ -31,11 +31,6 @@ public class GrantTrackDbContext : DbContext
     public DbSet<Report> Reports{get; set;}
     public DbSet <Notification>notifications { get; set; }
     
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Server=LTIN718866\\SQLEXPRESS;Database=GrantTrack;Trusted_Connection=True;TrustServerCertificate=True");
-    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Application>()
