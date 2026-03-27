b3170f21c629fdaa902cb45156e0646d10d6bf6c
Sadayappan Arunachalam <sadayappan3101@gmail.com>
2026-03-27 16:26:30 +0530

    Updated changes

--- GrantTrack/Repository/UserRepositories/UserRepository.cs
+++ GrantTrack/Repository/UserRepositories/UserRepository.cs
@@ -6,5 +6,10 @@ namespace GrantTrack.Repository.UserRepositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly GrantTrackDbContext _context;
+    public UserRepository(GrantTrackDbContext context)
+    {
+        _context=context;
+    }
 
 }
