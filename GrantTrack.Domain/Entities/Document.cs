417460e2519db6c48bf128ceb9ecaaa29b0b268a
Adesh <t.adesh002@gmail.com>
2026-04-24 16:59:32 +0530

    Added ContentType to entity

--- GrantTrack.Domain/Entities/Document.cs
+++ GrantTrack.Domain/Entities/Document.cs
@@ -9,6 +9,7 @@ public class Document
     public string DocType { get; set; } = string.Empty;
     public string FileName { get; set; } = string.Empty;
     public string FileURI { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
     public string Hash { get; set; } = string.Empty;
     public int Version { get; set; } = 1;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
